0762074a068c8ff4c45d2bc565f6003f9b8ec1d7
Ripon19109 <78579465+19109@users.noreply.github.com>
2021-06-03 11:19:02 +0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+$(".owl-carousel").owlCarousel({
+
+    autoplay   :true,
+    loop       :true,
+    items      :2,
+    nav        :true,
+
+  });
+  $('.circlechart').circlechart();
+  
+
+
+
+ 
+ 
